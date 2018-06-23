b40d8cd6a6c92a2d9e328825a0d3e7fb27c5f67f
Alexander Wunschik <alex@wunschik.net>
2018-06-23 15:50:40 +0200

    show id and balance at the same time

--- src/beadnet.js
+++ src/beadnet.js
@@ -173,15 +173,27 @@ class Beadnet {
 				
 		nodeParent.append("text")
 			.style("stroke-width", 0.5)
-			.attr("class", "node-text")
+			.attr("class", "node-text-id")
 			.attr("stroke", opt.container.backgroundColor)
 			.attr("fill", opt.container.backgroundColor)
 			.attr("font-family", "sans-serif")
 			.attr("font-size", "15px")
-			.attr("y", "5px")
+			.attr("y", "0px")
 			.attr("text-anchor", "middle")
 			.attr("pointer-events", "none")
-			.text((d) => d[opt.nodes.text]);
+			.text((d) => d.id);
+
+		nodeParent.append("text")
+			.style("stroke-width", 0.5)
+			.attr("class", "node-text-balance")
+			.attr("stroke", opt.container.backgroundColor)
+			.attr("fill", opt.container.backgroundColor)
+			.attr("font-family", "sans-serif")
+			.attr("font-size", "12px")
+			.attr("y", "15px")
+			.attr("text-anchor", "middle")
+			.attr("pointer-events", "none")
+			.text((d) => d.balance);
 
 		nodeParent.call(this.behaviors.drag);
 
